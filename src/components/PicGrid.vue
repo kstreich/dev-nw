a4e15f6b58f23bf11d90cd64ea4a33348503e2d4
Kimberly Streicher <kimberlystreicher@gmail.com>
2019-02-03 19:12:02 -0700

    grid

--- src/components/PicGrid.vue
+++ src/components/PicGrid.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="picGrid">
 
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-1.jpg">
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-2.jpg">
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-3.jpg">
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-4.jpg">
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-5.jpg">
-    <img class="col-sm-6 noPadding" src="../assets/tiles/tile-6.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-1.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-2.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-3.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-4.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-5.jpg">
+    <img class="col-sm-6 col-6 noPadding" src="../assets/tiles/tile-6.jpg">
 
   </div>
 </template>
